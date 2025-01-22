e06538bd2d18cd8c21737cc8bad0420a5a9f7670
palecupid <egor-yakovlev2012@yandex.ru>
2025-01-22 20:11:56 +0300

    Added fixtures folder, updated nameimgid model, filled models with data

--- main/models.py
+++ main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class NameImgIdModel(models.Model):
     nmID = models.CharField(max_length=20, primary_key=True, unique=True, default='0')
-    name = models.CharField(max_length=50)
-    img_url = models.CharField(max_length=200)
+    vendorCode = models.CharField(max_length=50)
+    photos = models.CharField(max_length=200)
 
 class IdSizeModel(models.Model):
     nmID = models.ForeignKey(NameImgIdModel, to_field='nmID', on_delete=models.CASCADE)

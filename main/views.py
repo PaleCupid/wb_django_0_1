d96e643c74ff6df1018228f16883435588137322
palecupid <egor-yakovlev2012@yandex.ru>
2025-01-23 20:19:21 +0300

    Added jinja2 lib, added new template in jinja2 fold, created func in views, connected urls

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import NameImgIdModel, IdSizeModel
 
-# Create your views here.
+def items_sales(request):
+    data_1 = list(NameImgIdModel.objects.values())
+    data_2 = list(IdSizeModel.objects.values())
+    for item_1 in data_1:
+        for item_2 in data_2:
+            if (item_1['nmID'] == item_2['nmID_id']) and (item_2['sizes'] != 'ONE SIZE') and ('sizes' in item_2.keys()):
+                item_1['sizes'] = item_1.get('sizes', [])
+                item_1['sizes'].append(item_2['sizes'])
+    # print(data_1)
+    return render(request, 'items_sales.html', {'data': data_1})
